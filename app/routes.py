b09907b2df624ffaf14b59b4cab5359e8ac78d78
Drew Ku <andrewku123@gmail.com>
2020-02-29 12:31:28 -0500

    requirements.txt

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,10 @@
 from flask import render_template, request
 import requests
 import json
-import configparser
 from app import app
 from app.forms import SearchSpotifyform
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST']) 
 def index():
 
     Spotifyform=SearchSpotifyform()
@@ -16,5 +15,7 @@ def index():
                                Spotifyform=Spotifyform)
     elif request.method == 'POST':
 
-        return render_template('result.html')
+        if Spotifyform.validate():
+            return render_template('result.html',
+                                   )
 
